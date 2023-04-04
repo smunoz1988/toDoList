dfbb5aa5a4afa7715b7f116e623a253027afa9c3
smunoz1988 <64698693+smunoz1988@users.noreply.github.com>
2023-04-03 21:47:25 -0500

    Update functionality.js

--- src/modules/functionality.js
+++ src/modules/functionality.js
@@ -1,38 +1,132 @@
+import { checkedBox } from "./check.js";
+
 export default class TaskList {
   constructor() {
-    this.list = [];
-    this.length = 0;
-    const storedList = localStorage.getItem('list');
-    if (storedList) {
-      const parsedList = JSON.parse(storedList);
-      this.length = parsedList[parsedList.length - 1].index;
-    }
+    this.tasks = JSON.parse(localStorage.getItem('data')) || [];
+    this.form = document.querySelector('form');
+    this.taskList = document.getElementById('container');
+    this.form.addEventListener('submit', this.addTask.bind(this));
+    this.renderTaskList();
+    this.checkbox();
   }
 
-  renderTask() {
-    const listGroup = document.querySelector('.container');
-    listGroup.innerHTML = '';
-    this.list.forEach((task) => {
-      const listTask = document.createElement('li');
-      listTask.classList = 'listItem listElement';
-      listTask.id = `${task.index}`;
-      listTask.innerHTML = `
-        <input type="checkbox" class="checkBox" ${task.completed ? 'checked' : ''}>
-        <input type="text" class="toDoTask" value="${task.description}">
-        <i class="fa-regular fa-trash-can removeIcon"></i>
-      `;
+  /* eslint-disable */
+  generateTaskHTML(task) {
+    const taskId = `task-${task.index}`;
+    const bodyInt = `<li class='listItems' id='${taskId}'>
+      <div class='itemDescriptionContainer'>
+        <input class="checkbox" type='checkbox' ${task.completed ? 'checked' : ''} data-task-id='${taskId}'>
+        <input type='text' class='itemDescription' value='${task.description}' data-task-id='${taskId}'>
+      </div>
+      <button class="removeItem" type="button" data-task-id='${taskId}'><i class="fa-solid fa-trash-can"></i></button>
+    </li>`;
+    return bodyInt;
+  }
+  /* eslint-disable */
 
-      this.checkBoxChanger(listTask, task);
+  renderTaskList() {
+    this.taskList.innerHTML = '';
+    this.tasks.forEach((task) => {
+      const listItem = this.generateTaskHTML(task);
+      this.taskList.innerHTML += listItem;
+    });
+    this.addDeleteListeners();
+    this.checkbox();
+    this.deleteAllCompleted();
+  }
 
-      const clearButton = document.querySelector('.clear');
-      clearButton.addEventListener('click', () => {
-        this.clearCompleted();
+  checkbox() {
+    checkedBox(this.tasks);
+  }
+
+  deleteAllCompleted() {
+    const deleteCompletedBtn = document.getElementById('clearAllButton');
+    deleteCompletedBtn.addEventListener('click', () => {
+      this.tasks = this.tasks.filter((task) => !task.completed);
+      localStorage.setItem('data', JSON.stringify(this.tasks));
+      this.renderTaskList();
+      console.log(this.tasks);
+    })
+  }
+
+  addTask(event) {
+    event.preventDefault();
+    const input = document.getElementById('inputTask');
+    const inputValue = input.value.trim();
+  
+    if (!inputValue) {
+      return;
+    }
+  
+    const newTask = {
+      description: inputValue,
+      completed: false,
+      index: this.tasks.length,
+    };
+  
+    this.tasks.push(newTask);
+    localStorage.setItem('data', JSON.stringify(this.tasks));
+    this.renderTaskList();
+    input.value = '';
+    this.checkbox();
+  }
+  
+
+  addDeleteListeners() {
+    const deleteButtons = document.querySelectorAll('.removeItem');
+    deleteButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        const taskId = button.getAttribute('data-task-id');
+        this.removeTask(taskId);
+        this.checkbox();
       });
+    });
 
-      listGroup.appendChild(listTask);
+    const inputElements = document.querySelectorAll('.itemDescription');
+    inputElements.forEach((input) => {
+      input.addEventListener('change', () => {
+        const taskId = input.getAttribute('data-task-id');
+        const taskIndex = parseInt(taskId.split('-')[1], 10);
+        this.tasks[taskIndex].description = input.value;
+        localStorage.setItem('data', JSON.stringify(this.tasks));
+        this.checkbox();
+      });
     });
   }
 
+  removeTask(taskId) {
+    const taskElement = document.getElementById(taskId);
+    const taskIndex = parseInt(taskId.split('-')[1], 10);
+    this.tasks.splice(taskIndex, 1);
+    taskElement.remove();
+  
+    // Update the data-task-id attribute of each checkbox element
+    const checkboxes = document.querySelectorAll('.checkbox');
+    checkboxes.forEach((checkbox, index) => {
+      const taskId = `task-${index}`;
+      checkbox.setAttribute('data-task-id', taskId);
+    });
+  
+    // Update index property of tasks
+    this.tasks.forEach((task, index) => {
+      task.index = index;
+    });
+  
+    localStorage.setItem('data', JSON.stringify(this.tasks));
+    this.refreshTaskIds();
+    this.renderTaskList();
+  }
+  
+  refreshTaskIds() {
+    const taskElements = document.querySelectorAll('.listItems');
+    taskElements.forEach((taskElement, index) => {
+      const taskId = `task-${index}`;
+      taskElement.setAttribute('id', taskId);
+      const checkbox = taskElement.querySelector('.checkbox');
+      checkbox.setAttribute('data-task-id', taskId);
+    });
+  }
+}
   addTask() {
     const addTask = document.querySelector('#toDo');
     const addInput = document.querySelector('.addToInput');
