dc7e44111f8e6356dd6ad7a85b09ca38f3caade6
smunoz1988 <64698693+smunoz1988@users.noreply.github.com>
2023-04-03 21:45:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import './style.css';
 import TaskList from './modules/functionality.js';
 
 const taskList = new TaskList();
-taskList.loadTask();
-taskList.addTask();
-taskList.removeTask();
-taskList.editTask();
-taskList.renderTask();
+window.onload = taskList.renderTaskList();
